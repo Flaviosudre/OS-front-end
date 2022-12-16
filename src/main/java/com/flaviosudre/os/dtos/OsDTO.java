f854fdeee115343830db22ede74d63fe2d503b95
Flavio Sudre <flaviosudre@gmail.com>
2022-12-16 02:55:34 -0300

    Controle de OS (Portifolio)

---
+++ src/main/java/com/flaviosudre/os/dtos/OsDTO.java
@@ -0,0 +1,119 @@
+package com.flaviosudre.os.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+import javax.validation.constraints.NotEmpty;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.flaviosudre.os.domain.OS;
+import com.flaviosudre.os.domain.enuns.Prioridade;
+import com.flaviosudre.os.domain.enuns.Status;
+
+public class OsDTO implements Serializable{
+	private static final Long serialVersionUID = 1L;
+
+	
+	private Integer id;
+	
+	@JsonFormat(pattern = "dd/MM/YYYY HH:mm")
+	private LocalDateTime dataAbertura;
+	
+	@JsonFormat(pattern = "dd/MM/YYYY HH:mm")
+	private LocalDateTime dataFechamento;
+	
+	private Integer prioridade;
+	private Integer status;
+	
+	@NotEmpty(message = "O campo é obrigatório.")
+	private String observacoes;
+	
+	private Integer tecnico;
+	private Integer cliente;
+
+	public OsDTO() {
+	}
+
+	public OsDTO(OS obj){
+		this.id = obj.getId();
+		this.dataAbertura = obj.getDataAbertura();
+		this.dataFechamento = obj.getDataFechamento();
+		this.prioridade = obj.getPrioridade().getCod();
+		this.status = obj.getStatus().getCod();
+		this.observacoes = obj.getObservacoes();
+		this.tecnico = obj.getTecnico().getId();
+		this.cliente = obj.getCliente().getId();
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataAbertura() {
+		return dataAbertura;
+	}
+
+	public void setDataAbertura(LocalDateTime dataAbertura) {
+		this.dataAbertura = dataAbertura;
+	}
+
+	public LocalDateTime getDataFechamento() {
+		return dataFechamento;
+	}
+
+	public void setDataFechamento(LocalDateTime dataFechamento) {
+		this.dataFechamento = dataFechamento;
+	}
+
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
+	}
+
+	public void setPrioridade(Integer prioridade) {
+		this.prioridade = prioridade;
+	}
+
+	public Status getStatus() {
+		return Status.toEnum(this.status);
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public String getObservacoes() {
+		return observacoes;
+	}
+
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+
+	public Integer getTecnico() {
+		return tecnico;
+	}
+
+	public void setTecnico(Integer tecnico) {
+		this.tecnico = tecnico;
+	}
+
+	public Integer getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Integer cliente) {
+		this.cliente = cliente;
+	}
+
+	public static Long getSerialversionuid() {
+		return serialVersionUID;
+	}
+	
+	
+
+	
+}
